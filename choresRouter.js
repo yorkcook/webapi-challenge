496b4989cb3f273f258a8ca181172797646887c8
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-10 15:17:15 -0400

    WIP: Initial Commit

---
+++ choresRouter.js
@@ -0,0 +1,20 @@
+const express = "express";
+
+const router = require("express").Router();
+
+let chores = [];
+
+let people = [
+    {
+        id: 1,
+        name: "York Cook"
+    },
+     {
+        id: 2,
+        name: "Lauren King"
+    }
+     {
+        id: 3,
+        name: "Team YoLo"
+    }
+]
---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const server = require("./server");
+
+const port = process.env.PORT || 4001;
+
+server.listen(port, () => {
+  console.log(`Server reporting for duty from port ${port}!`);
+});
