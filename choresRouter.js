2636c7f9f98e7564e7118ef07171c3f502c7eb3a
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-10 15:29:11 -0400

    WIP: basic endpoints made

--- choresRouter.js
+++ choresRouter.js
@@ -5,16 +5,26 @@ const router = require("express").Router();
 let chores = [];
 
 let people = [
-    {
-        id: 1,
-        name: "York Cook"
-    },
-     {
-        id: 2,
-        name: "Lauren King"
-    }
-     {
-        id: 3,
-        name: "Team YoLo"
-    }
-]
+  {
+    id: 1,
+    name: "York Cook"
+  },
+  {
+    id: 2,
+    name: "Lauren King"
+  },
+  {
+    id: 3,
+    name: "Team YoLo"
+  }
+];
+
+router.delete("/:id", (req, res) => {});
+
+router.get("/", (req, res) => {});
+
+router.post("/chore", (req, res) => {});
+
+router.put("/chore/:id", (req, res) => {});
+
+module.exports = router;
